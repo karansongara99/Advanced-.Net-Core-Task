5b0274ea642a0738893cc10ba021845d50ca5171
Karan Songara <karansongara290@gmail.com>
2025-06-12 08:11:50 +0530

    Configuration File Database

---
+++ Hospital Management API Task/Program.cs
@@ -0,0 +1,34 @@
+using Hospital_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+///Video Added
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<ClassTaskContext>(item => item.UseSqlServer(config.GetConnectionString("Connection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
