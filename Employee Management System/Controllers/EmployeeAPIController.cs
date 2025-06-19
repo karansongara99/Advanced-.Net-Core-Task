b0faa3f078155397703fca21d44a31cea541adb0
Karan Songara <karansongara290@gmail.com>
2025-06-19 08:35:47 +0530

    Models And Controllers

---
+++ Employee Management System/Controllers/EmployeeAPIController.cs
@@ -0,0 +1,62 @@
+﻿using Employee_Management_System.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Employee_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeAPIController : ControllerBase
+    {
+        [HttpGet("success")]
+        [HttpGet]
+        public IActionResult GetSuccess()
+        {
+            return Ok(new { message = "Success" });
+        }
+
+        [HttpGet("fail")]
+        public IActionResult GetFailure()
+        {
+            throw new Exception("Failaure Please Solve the error");
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] EmployeeModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var jsonOutput = JsonConvert.SerializeObject(employee, Formatting.Indented);
+
+                return Ok(new
+                {
+                    Message = "Employee Data Received",
+                    Data = jsonOutput
+                });
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public IActionResult DeserializeJson([FromBody] string json)
+        {
+            try
+            {
+                var employee = JsonConvert.DeserializeObject<EmployeeModel>(json);
+                return Ok(new
+                {
+                    Message = "Employee Data Received",
+                    EmployeeModel = employee
+                });
+            }
+            catch (Exception ex) {
+
+                return BadRequest(new
+                {
+                    Message = "Invalid JSON Format",
+                    EmployeeModel = ex
+                });
+            }
+        }  
+    }
+}
---
+++ Employee Management System/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+
+namespace Employee_Management_System.Models
+{
+    public class EmployeeModel
+    {
+            public int EmployeeID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            [Newtonsoft.Json.JsonIgnore]
+            public string PhoneNumber { get; set; }
+            public bool IsActive { get; set; }
+            [JsonProperty(NullValueHandling = NullValueHandling.Include)]
+            public DateTime DateOfBirth { get; set; }
+    }
+}
