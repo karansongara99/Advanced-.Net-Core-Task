b25d5b620a3cfe9eeb14e6c730421b677f4e9fec
Karan Songara <karansongara290@gmail.com>
2025-06-19 08:37:45 +0530

    Fronted Model Controller
    But Only Design of MVC

---
+++ Ecommerce Admin Panel/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Admin_Panel.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
